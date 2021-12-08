d4cd150233d4a187d92ee7281f2bd72853ac1755
yappy727 <yappy727@gmail.com>
2021-12-08 13:30:08 +0900

    first commit

---
+++ src/main/java/io/yamory/App.java
@@ -0,0 +1,13 @@
+package io.yamory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
